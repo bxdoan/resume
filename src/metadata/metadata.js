88a5a6172d914d401e4e09c5c7884f42f16ec02c
Doan Bui <bxdoan93@gmail.com>
2025-01-18 09:38:45 +0700

    setup

---
+++ src/metadata/metadata.js
@@ -0,0 +1,110 @@
+module.exports = {
+  name: "Bui Xuan Doan",
+  title: "Senior Software Engineer",
+  baseUrl: "https://bxdoan.github.io/resume",
+  facts: {
+    GitHub:
+      '<a href="https://github.com/bxdoan"><i class="fa-brands fa-github fact-icon"></i></i>Github</a>',
+    LinkedIn:
+      '<a href="https://www.linkedin.com/in/bxdoan"><i class="fa-brands fa-linkedin fact-icon"></i>LinkedIn</a>',
+    Stackoverflow:
+      '<a href="https://stackoverflow.com/users/6399715/bxdoan"><i class="fa-brands fa-stack-overflow fact-icon"></i></i>Stackoverflow</a>',
+    Email:
+      '<a href="mailto:bxdoan93@gmail.com"><i class="fa-solid fa-envelope fact-icon"></i>bxdoan93@gmail.com</a>',
+    Phone:
+      '<a href="#"><i class="fa-solid fa-phone fact-icon"></i></i>0904195065</a>',
+  },
+  skills: [
+    ["Python"],
+    ["Bash Script"],
+    ["Javascript"],
+    ["ELK Stack"],
+    ["AWS"],
+    ["CI/CD"],
+    ["FluxCD"],
+    ["GitOps"],
+    ["Kubernetes"],
+  ],
+  summary: `
+    <div>Experienced Software Engineer with 8+ years of proven expertise in designing and building scalable, high-performance web applications.</div>
+    <div>Hands-on experience in microservices architecture, CI/CD pipelines, GitOps workflows, and Kubernetes orchestration for reliable, scalable deployments.</div>
+    `,
+  positions: [
+    {
+      company: "Gigacover",
+      link: "https://www.gigacover.com",
+      location: "",
+      title: "Senior Software Engineer",
+      period: "Sep 2019 - Now",
+      skills: [
+        "Python",
+        "Falcon framework",
+        "Flask framework",
+        "Bash Script",
+        "Alchemy",
+        "CircleCI",
+        "PostgreSQL",
+        "GitHub Actions",
+        "MongoDB",
+        "Redis",
+        "Celery",
+        "Selenium",
+      ],
+      contents: `
+Insurance for freelancers.
+
+Key Responsibilities and Achievements:
+
+* Implement new feature based on new customers.
+* Write Unit Test, Block Test, and Function Test, UX/UI test to verify new features.
+* Documenting new features for the Product Manager.
+* Troubleshooting and debugging trouble reports.
+* Implementing internal tools.
+* Report process daily to Product Manager
+* Build docker and script support CI (circleCI and Jenkin)
+* Build ChatAI support customers using power chatGPT
+* Integrate with other system (moengage, clevertap,
+sendgrid...) or payment (stripe, xendit)
+`,
+    },
+    {
+      company: "DEK TECHNOLOGIES",
+      link: "#",
+      location: "",
+      title: "Software Engineer",
+      period: "Jan 2016 - Aug 2019",
+      skills: [
+        "Erlang",
+        "Shell Script",
+        "Python Script",
+        "React.js",
+        "Jenkins",
+        "Mnesia Database",
+      ],
+      contents: `
+Session Border Gateway(SBG)
+
+Key Responsibilities and Achievements:
+
+* Implement new feature
+* Write Unit Test, Block Test, Function Test to verify new features
+* Documenting new features for the Product Manager
+* Troubleshooting and debugging trouble reports
+* Implementing internal tools
+* Report process daily to Product Manager
+* Build docker and script support CI (Jenkin)
+`,
+    },
+  ],
+  education: [
+    {
+      period: "2014 &ndash; 2018",
+      contents: `<b>Bachelor of Telecommunication and Electronics</b> at HCMC University of Science`,
+    },
+    {
+      period: "2019",
+      contents: `IELTS 6`,
+    },
+  ],
+  others: [],
+};
